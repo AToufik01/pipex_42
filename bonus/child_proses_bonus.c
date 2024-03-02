ffc6d69305af93f256a919e450c22bfd78beec99
AToufik01 <simotaoufiki99@gmail.com>
2024-03-02 19:28:55 +0100

    add new v1

--- bonus/child_proses_bonus.c
+++ bonus/child_proses_bonus.c
@@ -6,79 +6,93 @@
 /*   By: ataoufik <ataoufik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 18:49:20 by ataoufik          #+#    #+#             */
-/*   Updated: 2024/03/01 16:46:26 by ataoufik         ###   ########.fr       */
+/*   Updated: 2024/03/02 19:28:38 by ataoufik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "../pipex.h"
+#include "../pipex_bonus.h"
 
-// void    free_2d_arr(char **str)
-// {
-//     int i;
-//     i = 0;
-//     while(str[i])
-//         free(str[i++]);
-//     free(str);
-// }
-// void    process_child_f(t_pipex *pip, char *cmd1)
-// {
-//     int infile;
-//     char    *command;
+void    process_child_f_m(t_pipexb *pip, int i)
+{
+    int fd[2];
+    int file = 0;
+    if (pipe(fd)== -1)
+        printf("errrrororor hnaya fi process child_f_m");
+    if (i == 2)
+        file = open(pip->args[2], O_RDONLY);
+    else if (i == 3)
+        file = open(pip->args[pip->n - 1],O_WRONLY,O_CREAT,O_APPEND);
+    if (file < 0)
+        exit(1);
+    pip->pid = fork();
+    if (pip->pid == 0)
+    {
+        char *command;
+        pip->cmd = ft_split(pip->args[i],' ');
+        close(fd[0]);
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[1]);
+        command = find_path_executable(pip, pip->cmd[0]);
+        if (command == NULL)
+        {
+            // perror("path");
+            exit(1);
+        }
+        execve(command, pip->cmd, NULL);
+        free_2d_arr(pip->cmd);
+        free(command);
+    }
+    else
+    {
+        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO);
+        close(fd[0]);
+    }
+}
 
-//     infile = open(pip->args[1],O_RDONLY);
-//     if (infile < 0)
-//         exit(1);
-//     close(pip->fd[0]);
-//     dup2(infile, STDIN_FILENO);
-//     close(infile);
-//     dup2(pip->fd[1], STDOUT_FILENO);
-//     close(pip->fd[1]);
-//     command = find_path_executable(pip,cmd1);
-//     if (command == NULL)
-//     {
-//         perror("path");
-//         exit(1);
-//     }
-//     execve(command, pip->cmd1, NULL);
-// }
+void    process_child_last(t_pipexb *pip)
+{
+    int outfile;
+    char *command;
+    pip->pid = fork();
+    if (pip->pid == 0)
+    {
+        outfile = open(pip->args[pip->n - 1],O_WRONLY,O_CREAT,O_TRUNC);
+        if (outfile < 0)
+            exit(1);
+        pip->cmd = ft_split(pip->args[pip->n - 1],' ');
+        dup2(outfile,STDIN_FILENO);
+        close(outfile);
+        command = find_path_executable(pip,pip->cmd[0]);
+        if (command == NULL)
+        {
+            // perror("path");
+            exit(1);
+        }
+        execve(command, pip->cmd, NULL);
+        free_2d_arr(pip->cmd);
+        free(command);
+    }
+}
 
-// void    process_child_l(t_pipex *pip, char *cmd2)
-// {
-//     int outfile;
-//     char    *command;
+char    *find_path_executable(t_pipexb *pip, char *cmd)
+{
+    char    *str;
+    char    *path;
+    int i;
 
-//     outfile = open(pip->args[pip->n - 1], O_WRONLY | O_CREAT | O_TRUNC, 0666);
-//     if (outfile < 0)
-//         exit(1);
-//     close(pip->fd[1]);
-//     dup2(outfile, STDOUT_FILENO);
-//     close(outfile);
-//     dup2(pip->fd[0], STDIN_FILENO);
-//     close(pip->fd[0]);
-//     command = find_path_executable(pip,cmd2);
-//     printf("%s\n",command);
-//     if (command == NULL)
-//     {
-//         perror("path");
-//         exit(1);
-//     }
-//     execve(command, pip->cmd2, NULL);
-// }
-
-// void    process_child_m(t_pipex *pip, char *cmd)
-// {
-//     char    *command;
-//     close(pip->fd[1]);
-//     dup2(outfile, STDOUT_FILENO);
-//     close(outfile);
-//     dup2(pip->fd[0], STDIN_FILENO);
-//     close(pip->fd[0]);
-//     command = find_path_executable(pip,cmd2);
-//     printf("%s\n",command);
-//     if (command == NULL)
-//     {
-//         perror("path");
-//         exit(1);
-//     }
-//     execve(command, pip->cmd2, NULL);
-// }
+    i = 0;
+    str = NULL;
+    path = NULL;
+    while(pip->env_path[i])
+    {
+        str = ft_strjoin(pip->env_path[i], "/");
+        path = ft_strjoin(str, cmd);
+        free(str);
+        if (access(path, X_OK) == 0)
+            return (path);
+        i++;
+        free(path);
+    }
+    return NULL;
+}
--- bonus/pipex_main_bonus.c
+++ bonus/pipex_main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: ataoufik <ataoufik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/24 16:06:43 by ataoufik          #+#    #+#             */
-/*   Updated: 2024/03/02 18:45:20 by ataoufik         ###   ########.fr       */
+/*   Updated: 2024/03/02 19:28:25 by ataoufik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,27 +29,7 @@ void    inist_pipe(t_pipexb *pip,int arc, char *arv[],char *evm[])
     pip->args = arv;
     pip->n = arc;
 }
-char    *find_path_executable(t_pipexb *pip, char *cmd)
-{
-    char    *str;
-    char    *path;
-    int i;
 
-    i = 0;
-    str = NULL;
-    path = NULL;
-    while(pip->env_path[i])
-    {
-        str = ft_strjoin(pip->env_path[i], "/");
-        path = ft_strjoin(str, cmd);
-        free(str);
-        if (access(path, X_OK) == 0)
-            return (path);
-        i++;
-        free(path);
-    }
-    return NULL;
-}
 void    ft_here_doc(t_pipexb *pip, char *str)
 {
 
@@ -72,68 +52,7 @@ void    ft_here_doc(t_pipexb *pip, char *str)
         pip->i = 2;
 }
 
-void    process_child_f_m(t_pipexb *pip, int i)
-{
-    int fd[2];
-    int file = 0;
-    if (pipe(fd)== -1)
-        printf("errrrororor hnaya fi process child_f_m");
-    if (i == 2)
-        file = open(pip->args[2], O_RDONLY);
-    else if (i == 3)
-        file = open(pip->args[pip->n - 1],O_WRONLY,O_CREAT,O_APPEND);
-    if (file < 0)
-        exit(1);
-    pip->pid = fork();
-    if (pip->pid == 0)
-    {
-        char *command;
-        pip->cmd = ft_split(pip->args[i],' ');
-        close(fd[0]);
-        dup2(fd[1], STDOUT_FILENO);
-        close(fd[1]);
-        command = find_path_executable(pip, pip->cmd[0]);
-        if (command == NULL)
-        {
-            // perror("path");
-            exit(1);
-        }
-        execve(command, pip->cmd, NULL);
-        free_2d_arr(pip->cmd);
-        free(command);
-    }
-    else
-    {
-        close(fd[1]);
-        dup2(fd[0], STDIN_FILENO);
-        close(fd[0]);
-    }
-}
 
-void    process_child_last(t_pipexb *pip)
-{
-    int outfile;
-    char *command;
-    pip->pid = fork();
-    if (pip->pid == 0)
-    {
-        outfile = open(pip->args[pip->n - 1],O_WRONLY,O_CREAT,O_TRUNC);
-        if (outfile < 0)
-            exit(1);
-        pip->cmd = ft_split(pip->args[pip->n - 1],' ');
-        dup2(outfile,STDIN_FILENO);
-        close(outfile);
-        command = find_path_executable(pip,pip->cmd[0]);
-        if (command == NULL)
-        {
-            // perror("path");
-            exit(1);
-        }
-        execve(command, pip->cmd, NULL);
-        free_2d_arr(pip->cmd);
-        free(command);
-    }
-}
 int main(int arc, char *arv[], char *evm[])
 {
     t_pipexb    pip;
